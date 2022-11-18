ee0951ec125fed474ee31ac38732413a351b4149
edris <edris.emperator@gmail.com>
2022-11-18 15:21:24 +0330

    add result page

--- components/utility/search/Search.tsx
+++ components/utility/search/Search.tsx
@@ -1,34 +1,39 @@
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 
 export interface ISearch {}
 
-const BaseTemplate: React.FC<ISearch> = ({}) => {
-  const [searchTerm, setSearch] = useState<string>();
+const Search: React.FC<ISearch> = () => {
+  const router = useRouter();
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
   return (
     <form
-      className="flex flex-col items-center gap-y-5 "
+      className="flex flex-col items-center gap-y-5"
       onSubmit={(e) => {
         e.preventDefault();
-        alert(searchTerm);
+        router.push(`/results?search=${searchTerm}`);
       }}
     >
       <input
         type="text"
-        name="search"
-        className="rounded-full border-2  w-5/6 sm:w-96 h-12 px-3"
+        className="rounded-full border-2 w-5/6 sm:w-128 h-12 px-3"
         value={searchTerm}
-        onChange={(e) => setSearch(e.target.value)}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <div className="space-x-3">
         <button type="submit" className="btn-primary">
           Google Search
         </button>
-        <button type="submit" className="btn-primary">
-          I'm Feeling Lucky
+        <button
+          onClick={() => alert('FEATURE COMING SOON!')}
+          className="btn-primary"
+        >
+          I&apos;m Feeling Lucky
         </button>
       </div>
     </form>
   );
 };
 
-export default BaseTemplate;
+export default Search;
